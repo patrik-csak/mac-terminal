7e0a8b50f26de3c5bc7101ec9ea2292e8d57878c
Patrik Csak <p@trikcsak.com>
2026-04-17 19:46:00 -0700

    fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,21 @@ import packageJson from 'eslint-plugin-package-json';
 import xo from 'xo';
 
 export default defineConfig([
-	packageJson.configs.recommended,
-	packageJson.configs.stylistic,
-	...xo.xoToEslintConfig([{prettier: 'compat'}]),
+	{
+		files: ['**/*.js'],
+		extends: [
+			xo.xoToEslintConfig([
+				{
+					prettier: 'compat',
+				},
+			]),
+		],
+	},
+	{
+		files: ['package.json'],
+		extends: [packageJson.configs.recommended, packageJson.configs.stylistic],
+		rules: {
+			'package-json/require-sideEffects': 'off',
+		},
+	},
 ]);
